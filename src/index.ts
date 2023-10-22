4bd41d2e61c2d0ff540992a9c669e7b5754eba76
János Illés <ijanos@gmail.com>
2023-10-22 17:29:35 +0200

    small cleanups of ts code
    init ffmpeg async among other things

--- src/index.ts
+++ src/index.ts
@@ -1,63 +1,59 @@
 import { FFmpeg } from '@ffmpeg/ffmpeg';
 import { fetchFile, toBlobURL } from '@ffmpeg/util';
 
-let ffmpeg: FFmpeg|null = null;
-let name: string|null = null;
-
 const initFFmpeg = async () => {
-  ffmpeg = new FFmpeg();
-    ffmpeg.on("log", ({ message }) => {
-      console.log(message);
-    })
+  const ffmpeg = new FFmpeg();
 
-    await ffmpeg.load({
-      coreURL: await toBlobURL('/ffmpeg-core.js', "text/javascript"),
-      wasmURL: await toBlobURL('/ffmpeg-core.wasm', "application/wasm")
-    });
+  ffmpeg.on("log", ({ message }) => {
+    console.log(message);
+  });
+
+  await ffmpeg.load({
+    coreURL: await toBlobURL('/ffmpeg-core.js', "text/javascript"),
+    wasmURL: await toBlobURL('/ffmpeg-core.wasm', "application/wasm")
+  });
+
+  return ffmpeg;
 }
 
-const transcode = async () => {
+const ffmpeg: Promise<FFmpeg> = initFFmpeg();
+let name: string = "";
+
+const dither = async () => {
   const dither = (document.getElementById('dither') as HTMLInputElement).value;
   const colors = (document.getElementById('colors') as HTMLInputElement).value;
 
-  if (ffmpeg === null) {
-    await initFFmpeg()
-  }
-
-  if (name == null ) { name = "" }
+  const ff = await ffmpeg;
 
   if (colors == "gen") {
-    await ffmpeg?.exec(['-i', name, '-vf', 'palettegen=max_colors=4', 'palette.png']);
+    await ff.exec(['-i', name, '-vf', 'palettegen=max_colors=4', 'palette.png']);
   } else {
-    await ffmpeg?.writeFile("palette.png", await fetchFile(`pal_${colors}.png`));
+    await ff.writeFile("palette.png", await fetchFile(`pal_${colors}.png`));
   }
 
-  await ffmpeg?.exec(['-i', name, '-i', 'palette.png', '-lavfi', `paletteuse=dither=${dither}`, "output.png"]);
+  await ff.exec(['-i', name, '-i', 'palette.png', '-lavfi', `paletteuse=dither=${dither}`, "output.png"]);
 
-  const data = await ffmpeg?.readFile('output.png') as Uint8Array;
+  const data = ff.readFile('output.png') as Promise<Uint8Array>;
 
   const image = document.getElementById('output-image') as HTMLImageElement;
-  image.src = URL.createObjectURL(new Blob([data.buffer], { type: 'image/png' }));
+  image.src = URL.createObjectURL(new Blob([(await data).buffer], { type: 'image/png' }));
 }
 
-const updateThumbnail = async ( e: Event ) : Promise<void> => {
+const updateThumbnail = async (e: Event): Promise<void> => {
   const target = e.target as HTMLInputElement;
   const file = (target.files as FileList)[0];
-
-  if (ffmpeg === null) {
-    await initFFmpeg()
-  }
+  const ff = await ffmpeg;
 
   name = file.name;
-  await ffmpeg?.writeFile(name, await fetchFile(file));
+  await ff.writeFile(name, await fetchFile(file));
 
-  const data = await ffmpeg?.readFile(name) as Uint8Array;
   const image = document.getElementById('thumbnail') as HTMLImageElement;
 
-  image.src = URL.createObjectURL(new Blob([data.buffer]));
+  image.src = URL.createObjectURL(file);
 }
 
-const elm = document.getElementById('uploader') as HTMLInputElement;
-elm.addEventListener('change', updateThumbnail);
+const uploadButton = document.getElementById('uploader') as HTMLInputElement;
+uploadButton.addEventListener('change', updateThumbnail);
+
 const ditherbutton = document.getElementById('ditherbutton') as HTMLButtonElement;
-ditherbutton.addEventListener('click', transcode);
+ditherbutton.addEventListener('click', dither);
